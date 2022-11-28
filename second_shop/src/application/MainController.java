dd6f452c1bd03f2257f4b881bc545458777f98ff
Yifan Zou <zou.yifa@northeastern.edu>
2022-11-28 15:51:44 -0500

    Yifan Zou upload first files first

---
+++ second_shop/src/application/MainController.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class MainController {
+	@FXML 
+	private Label lblLogin;
+	
+	@FXML
+	private TextField txtUsername;
+	
+	@FXML
+	private TextField txtPassword;
+	
+	public void Login(ActionEvent event) {
+		if(txtUsername.getText().equals("user")&&txtPassword.getText().equals("pass")) {
+			lblLogin.setText("Login Success");}
+		else {
+			lblLogin.setText("Login Failed");
+		
+		}
+	}
+	
+	}
+
