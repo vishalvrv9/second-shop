1b6a81de3cbcbc75101c8167c27a14566a7c6c70
YiruFang <fang.yir@northeastern.edu>
2022-11-29 16:42:20 -0500

    add

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			primaryStage.setScene(scene);//add
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -27,4 +27,7 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	public void testMethod() {
+		  System.out.println("Just for testing purposes");
+		 }
 }
