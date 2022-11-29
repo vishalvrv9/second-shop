8166cfb4a1d4f3bb95bf5061a75d4ca760b9f8a3
Yifan Zou <zou.yifa@northeastern.edu>
2022-11-28 21:54:51 -0500

    fixed edition - Yifan

--- MyCircle/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module MyCircle {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
--- MyJavaFx/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module MyJavaFx {
-	requires javafx.controls;
-	requires java.desktop;
-	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
