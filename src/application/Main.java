4c2ac15539ff3916686d25529e1355b445c24801
Yifan Zou <zou.yifa@northeastern.edu>
2022-11-30 04:25:10 -0500

    add more details about login function,add user/admin system file

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
 public class Main extends Application {
@@ -16,9 +17,11 @@ public class Main extends Application {
             Parent root = loader.load();
 
             // adds comment to commit to git
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,500,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Second Shop System - best system for helping :) !");
+			primaryStage.getIcons().add(new Image("icon.png"));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,31 +1,81 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 public class MainController {
-	@FXML
+	@FXML 
 	private Label lblLogin;
-
+	
 	@FXML
 	private TextField txtUsername;
-
+	
 	@FXML
 	private TextField txtPassword;
-
-	public void Login(ActionEvent event) {
-		if (txtUsername.getText().equals("user") && txtPassword.getText().equals("pass")) {
-			lblLogin.setText("Login Success");
-		}
-
-		else {
-			//adding comment to show some test commit
+	
+	@FXML
+	private RadioButton userbutton;
+	
+	@FXML
+	private RadioButton adminbutton;
+	
+	public void Login(ActionEvent event) throws IOException {
+		
+		//set toggle group to confirm only one button to be selected primarily
+		final ToggleGroup group = new ToggleGroup();
+		userbutton.setToggleGroup(group);
+		userbutton.setSelected(true);
+		adminbutton.setToggleGroup(group);
+		
+		// admin login
+		if(txtUsername.getText().equals("admin")&&txtPassword.getText().equals("pass")&& adminbutton.isSelected()) {
+			lblLogin.setText("Admin Login Success");
+			Stage primaryStage=new Stage();
+			
+		// if successfully login, open admin system
+			FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("application/AdminSystem.fxml"));
+            Parent root = loader.load();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Hi! There is Admin System");
+			primaryStage.show();}
+		
+		// user login
+		else if(txtUsername.getText().equals("user")&&txtPassword.getText().equals("pass")&& userbutton.isSelected()) {
+			lblLogin.setText(" User Login Success");
+			Stage primaryStage=new Stage();
+			
+		// if successfully login, open user system
+			FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("application/UserSystem.fxml"));
+            Parent root = loader.load();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Hi! There is User System");
+			primaryStage.show();}
+		
+		else{
 			lblLogin.setText("Login Failed");
-			//testing
-
 		}
 	}
-
+	public void Cancel(ActionEvent event) throws IOException{
+		if( txtUsername.getText()!=null || txtPassword.getText()!=null) {
+			lblLogin.setText("You cancel your login");
+			txtUsername.setText("");
+			txtPassword.setText("");
+		}else{
+			lblLogin.setText("invalid input");
+		}
+	}
 }
