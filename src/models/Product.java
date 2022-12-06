7082805ad63c8d6dfb9a238f3530e6554ea475e3
Vishal Raj <vishalraj1802@gmail.com>
2022-12-05 22:03:08 -0500

    adds models package

---
+++ src/models/Product.java
@@ -0,0 +1,69 @@
+package models;
+
+public class Product {
+	
+	private int id;
+	private String name;
+	private String image;
+	private String type;
+	private String postedBy;
+	private String size;
+	private double price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getPostedBy() {
+		return postedBy;
+	}
+
+	public void setPostedBy(String postedBy) {
+		this.postedBy = postedBy;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
