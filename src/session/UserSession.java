c0b7afce9f9d8a967162e1a761548db26f276172
Vishal Raj <vishalraj1802@gmail.com>
2022-12-07 16:44:56 -0500

    adds session singleton

---
+++ src/session/UserSession.java
@@ -0,0 +1,42 @@
+package session;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Product;
+
+public class UserSession {
+	private static List<Product> userCart = new ArrayList<>();
+	
+	private static UserSession userSession;
+	
+	private UserSession() {
+		
+	}
+	
+	public static UserSession getInstance() {
+		if(userSession != null) {
+			return userSession;
+		}
+		
+		return new UserSession();
+	}
+
+	public List<Product> getUserCart() {
+		return userCart;
+	}
+
+	public void setUserCart(List<Product> userCart) {
+		UserSession.userCart = userCart;
+	}
+
+
+	@Override
+	public String toString() {
+		String result = "UserSession []";
+		result += "User Cart details: \n";
+		for(Product prod: userCart) {
+			result+= prod.toString();
+		}
+		return result;
+	}
+}
