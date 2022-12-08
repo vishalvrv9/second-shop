ccd141cad44424b6ec8cabb5f1b6ebec56050bb7
Vishal Raj <vishalraj1802@gmail.com>
2022-12-07 23:48:04 -0500

    init admin dashboard

--- src/controllers/AdminDashboardController.java
+++ src/controllers/AdminDashboardController.java
@@ -4,7 +4,11 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import models.Product;
 import models.User;
 import utils.ApprovalQueue;
@@ -13,6 +17,28 @@ public class AdminDashboardController implements Initializable{
 	
 	private List<Product> sellerQueue;
 	private List<User> userAuthQueue;
+	
+	@FXML
+	private Button btnOverview;
+	
+	@FXML
+	private Button btnApproveUsers;
+	
+	@FXML
+	private Button btnApproveSellers;
+	
+	@FXML
+	private Button btnLogout;
+	
+	@FXML
+	private Pane overviewPane;
+	
+	@FXML
+	private Pane approveUsersPane;
+	
+	@FXML
+	private Pane approveSellersPane;
+	
 	
 
 	@Override
@@ -24,4 +50,23 @@ public class AdminDashboardController implements Initializable{
 		System.out.println(sellerQueue);
 	}
 
+    public void handleClicks(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == btnOverview) {
+            overviewPane.setStyle("-fx-background-color : #1620A1");
+            overviewPane.toFront();
+        }
+        if (actionEvent.getSource() == btnApproveUsers) {
+            approveUsersPane.setStyle("-fx-background-color : #53639F");
+            approveUsersPane.toFront();
+        }
+        if (actionEvent.getSource() == btnApproveSellers) {
+            approveSellersPane.setStyle("-fx-background-color : #02030A");
+            approveSellersPane.toFront();
+        }
+        if(actionEvent.getSource()==btnLogout)
+        {
+            System.out.println("Logout admin");
+            
+        }
+    }
 }
