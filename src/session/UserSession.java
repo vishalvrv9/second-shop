a4b313c30c35c5668e935422285b08c4aee93d0d
Vishal Raj <vishalraj1802@gmail.com>
2022-12-08 07:17:21 -0500

    cart functionality working fine.

--- src/session/UserSession.java
+++ src/session/UserSession.java
@@ -1,7 +1,10 @@
 package session;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import models.Product;
 
 public class UserSession {
--- src/utils/ApprovalQueue.java
+++ src/utils/ApprovalQueue.java
@@ -1,48 +1,116 @@
 package utils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.scene.paint.Color;
 import models.Product;
 import models.User;
 
 public class ApprovalQueue {
+
+	Connection con = null;
+	PreparedStatement preparedStatement = null;
+	ResultSet resultSet = null;
 	
 	private static List<Product> sellerQueue = new ArrayList<>();
 	private static List<User> userAuthQueue = new ArrayList<>();
-	
+
 	private static ApprovalQueue queueInstance;
-	
+
 	private ApprovalQueue() {
+		con = ConnectionUtil.connectToDB();
+		fetchPendingSellers();
+		fetchPendingUserAdmins();
 	}
-	
+
 	public static ApprovalQueue getInstance() {
-		if(queueInstance == null) {
+		if (queueInstance == null) {
 			queueInstance = new ApprovalQueue();
 		}
-		
+
 		return queueInstance;
 	}
-	
-	public List<Product> getSellerQueue(){
+
+	public List<Product> getSellerQueue() {
 		return sellerQueue;
 	}
-	
 
-	public List<User> getUserAuthQueue(){
+	public List<User> getUserAuthQueue() {
 		return userAuthQueue;
 	}
+
+	private void fetchPendingSellers() {
+
+		String sqlQuery = "select * from products where isApproved=0";
+		try {
+			preparedStatement = con.prepareStatement(sqlQuery);
+
+			resultSet = preparedStatement.executeQuery();
+			if (!resultSet.isBeforeFirst()) {
+				System.out.println("No pending queue orders");
+			} else {
+				System.out.println(resultSet.toString());
+				while (resultSet.next()) {
+					Product prod = new Product();
+					prod.setId(resultSet.getInt("productid"));
+					prod.setName(resultSet.getString("name"));
+					prod.setImage(resultSet.getString("image"));
+					prod.setType(resultSet.getString("type"));
+					prod.setPostedBy(resultSet.getString("postedBy"));
+					prod.setSize(resultSet.getString("size"));
+					prod.setPrice(resultSet.getDouble("price"));
+
+					sellerQueue.add(prod);
+				}
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.err.println(ex.getMessage());
+		}
+	}
 	
-	
+	private void fetchPendingUserAdmins() {
+		String sqlQuery = "select * from admins where isApproved=0";
+		try {
+			preparedStatement = con.prepareStatement(sqlQuery);
+
+			resultSet = preparedStatement.executeQuery();
+			if (!resultSet.isBeforeFirst()) {
+				System.out.println("No pending queue orders");
+			} else {
+				System.out.println(resultSet.toString());
+				while (resultSet.next()) {
+					User user = new User();
+//					user.setId(resultSet.getInt("adminid"));
+					user.setUsername(resultSet.getString("username"));
+					user.setPassword(resultSet.getString("password"));
+					user.setFirstname(resultSet.getString("firstname"));
+					user.setLastname(resultSet.getString("lastname"));
+					user.setEmail(resultSet.getString("email"));
+
+					userAuthQueue.add(user);
+				}
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.err.println(ex.getMessage());
+		}
+	}
+
 	@Override
 	public String toString() {
 		String result = "ApprovalQueue \n: ";
 		result += "seller queue details: \n";
-		for(Product prod: sellerQueue) {
-			result+= prod.toString();
+		for (Product prod : sellerQueue) {
+			result += prod.toString();
 		}
 		result += "user auth queue details: \n";
-		for(User user: userAuthQueue) {
+		for (User user : userAuthQueue) {
 			result += user.toString();
 		}
 		return result;
